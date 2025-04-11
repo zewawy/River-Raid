211467bcb2554f5af362e3f835fe64ad47665212
shroukAli01 <Shroukali2006@gmail.com>
2025-04-11 18:45:15 +0200

    Add files via upload

---
+++ myFile.cpp
@@ -0,0 +1,31 @@
+#include "CMUgraphicsLib\CMUgraphics.h"
+
+#include "CMUgraphicsLib\auxil.h"	
+#include "fuelDepots.h"
+
+
+int main() {
+    window w(1200, 1200);
+
+
+    w.ChangeTitle("magic");
+ 
+
+    fuel(w,300,500);
+    //jets(w, 200, 300);
+   // helicopter(w, 100, 100);
+
+   
+
+}
+
+
+
+
+
+
+
+
+
+
+
