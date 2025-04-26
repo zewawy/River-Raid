3947dfe84def0bf5007fb65a808adf63323d3982
zewawy <s-kevin.heshmat@zewailcity.edu.eg>
2025-04-26 20:11:48 +0300

    background.h

--- background.h
+++ background.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "CMUgraphicsLib/CMUgraphics.h"
+#include <vector>
+using namespace std;
+
+struct Tree {
+    int x, y;
+};
 
 class Background {
 private:
     window* pWind;
     int offsetY;
+    vector<Tree> trees;
 
 public:
     Background(window* w);
