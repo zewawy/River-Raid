8386eb787c8f04f3938483bbb1569cbfc33d1d1f
shroukAli01 <Shroukali2006@gmail.com>
2025-05-12 18:54:04 +0300

    Update jet.h

--- jet.h
+++ jet.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "GameObject.h"
+#include "GameConfig.h"
 
 class Jet : public GameObject
 {
+private:
+    int speed;
+    int direction;  // +1: right, -1: left
+
 public:
-    Jet(Game* pGame, point ref);  // Constructor
-    void draw() const override;   // Draw function
+    Jet(Game* pGame, point ref, int spd );
+    void move();
+    void draw() const override;
 };
